2c45794fd83bb8aa04d85f4d4901052a5c030652
David Sodré Lins <david.silva@acpgroup.com.br>
2021-05-24 21:05:56 -0300

    Estrutura Básica da API - WIP

---
+++ B2sWapi/src/main/java/b2w/swapi/B2sWapi/B2sWapiApplication.java
@@ -0,0 +1,13 @@
+package b2w.swapi.B2sWapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2sWapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2sWapiApplication.class, args);
+	}
+
+}
---
+++ B2sWapi/src/test/java/b2w/swapi/B2sWapi/B2sWapiApplicationTests.java
@@ -0,0 +1,13 @@
+package b2w.swapi.B2sWapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2sWapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
